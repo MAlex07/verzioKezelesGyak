a5ee6d7afb90dc5f505aef881b80cecbafd4ba04
Petrik Diak <diak@petrik.hu>
2026-01-14 11:42:23 +0100

    elkészült az űrlap működése

---
+++ src/main.js
@@ -0,0 +1,23 @@
+function init(){
+  const form = document.getElementById("regisztracio")
+  const message = document.getElementById("message")
+  
+  form.addEventListener("submit", function(event){
+    event.preventDefault()
+
+    form.reset()
+
+    message.innerHTML = `
+        <div class="alert alert-success mt-3">
+            Sikeres regisztráció
+        </div>
+    `;
+  })
+
+}
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", init)
